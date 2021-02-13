074bb781cf367e5609600e5faa1b0d678f427dd2
Tyler Wisniewski <Tyler Wisniewski@DESKTOP-OQ52G1A>
2021-02-13 16:59:37 -0500

    Add my for loop i output of even numbers from 0-100

---
+++ src/assignment1_tylerwisniewski/Assignment1_TylerWisniewski.java
@@ -0,0 +1,13 @@
+
+package assignment1_tylerwisniewski;
+
+public class Assignment1_TylerWisniewski {
+
+    public static void main(String[] args) 
+    {
+for(int i=0; i<102; i+=2) // Initalizing variable i to 0.  i will keep increasing until it goes up to 100, by increments of 2. 
+ System.out.println(i); //Prints the output of the for loop
+        
+    }
+    
+}
